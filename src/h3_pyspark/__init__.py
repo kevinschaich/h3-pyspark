be9d0e401ca90c954854786a7f971ed57db011d8
Kevin Schaich <kschaich@palantir.com>
2021-11-24 19:00:12 -0500

    More verbose function signatures

--- src/h3_pyspark/__init__.py
+++ src/h3_pyspark/__init__.py
@@ -2,22 +2,7 @@ import h3
 from pyspark.sql import functions as F, types as T
 import json
 from inspect import getmembers, isfunction
-
-
-def sanitize_types(value):
-    """
-    PySpark does not support all the native types in Python, i.e. sets/tuples.
-    """
-    if isinstance(value, str) or isinstance(value, bool) or isinstance(value, int) or isinstance(value, float):
-        return value
-    if isinstance(value, set) or isinstance(value, tuple):
-        return [sanitize_types(v) for v in value]
-    if isinstance(value, list):
-        return [sanitize_types(v) for v in value]
-    if isinstance(value, dict):
-        return {k: sanitize_types(v) for k, v in value.items()}
-
-    return json.dumps(value)
+from utils import sanitize_types
 
 
 ###############################################################################
@@ -26,18 +11,18 @@ def sanitize_types(value):
 
 
 @F.udf(returnType=T.StringType())
-def geo_to_h3(*args, **kwargs):
-    return sanitize_types(h3.geo_to_h3(*args, **kwargs))
+def geo_to_h3(lat, lng, resolution):
+    return sanitize_types(h3.geo_to_h3(lat, lng, resolution))
 
 
 @F.udf(returnType=T.ArrayType(T.DoubleType()))
-def h3_to_geo(*args, **kwargs):
-    return sanitize_types(h3.h3_to_geo(*args, **kwargs))
+def h3_to_geo(h):
+    return sanitize_types(h3.h3_to_geo(h))
 
 
 @F.udf(returnType=T.ArrayType(T.ArrayType(T.DoubleType())))
-def h3_to_geo_boundary(*args, **kwargs):
-    return sanitize_types(h3.h3_to_geo_boundary(*args, **kwargs))
+def h3_to_geo_boundary(h, geo_json=False):
+    return sanitize_types(h3.h3_to_geo_boundary(h, geo_json))
 
 
 ###############################################################################
@@ -46,43 +31,43 @@ def h3_to_geo_boundary(*args, **kwargs):
 
 
 @F.udf(returnType=T.IntegerType())
-def h3_get_resolution(*args, **kwargs):
-    return sanitize_types(h3.h3_get_resolution(*args, **kwargs))
+def h3_get_resolution(h):
+    return sanitize_types(h3.h3_get_resolution(h))
 
 
 @F.udf(returnType=T.IntegerType())
-def h3_get_base_cell(*args, **kwargs):
-    return sanitize_types(h3.h3_get_base_cell(*args, **kwargs))
+def h3_get_base_cell(h):
+    return sanitize_types(h3.h3_get_base_cell(h))
 
 
 @F.udf(returnType=T.LongType())
-def string_to_h3(*args, **kwargs):
-    return sanitize_types(h3.string_to_h3(*args, **kwargs))
+def string_to_h3(h):
+    return sanitize_types(h3.string_to_h3(h))
 
 
 @F.udf(returnType=T.StringType())
-def h3_to_string(*args, **kwargs):
-    return sanitize_types(h3.h3_to_string(*args, **kwargs))
+def h3_to_string(h):
+    return sanitize_types(h3.h3_to_string(h))
 
 
 @F.udf(returnType=T.BooleanType())
-def h3_is_valid(*args, **kwargs):
-    return sanitize_types(h3.h3_is_valid(*args, **kwargs))
+def h3_is_valid(h):
+    return sanitize_types(h3.h3_is_valid(h))
 
 
 @F.udf(returnType=T.BooleanType())
-def h3_is_res_class_III(*args, **kwargs):
-    return sanitize_types(h3.h3_is_res_class_III(*args, **kwargs))
+def h3_is_res_class_III(h):
+    return sanitize_types(h3.h3_is_res_class_III(h))
 
 
 @F.udf(returnType=T.BooleanType())
-def h3_is_pentagon(*args, **kwargs):
-    return sanitize_types(h3.h3_is_pentagon(*args, **kwargs))
+def h3_is_pentagon(h):
+    return sanitize_types(h3.h3_is_pentagon(h))
 
 
 @F.udf(returnType=T.ArrayType(T.IntegerType()))
-def h3_get_faces(*args, **kwargs):
-    return sanitize_types(h3.h3_get_faces(*args, **kwargs))
+def h3_get_faces(h):
+    return sanitize_types(h3.h3_get_faces(h))
 
 
 ###############################################################################
@@ -91,53 +76,53 @@ def h3_get_faces(*args, **kwargs):
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def k_ring(*args, **kwargs):
-    return sanitize_types(h3.k_ring(*args, **kwargs))
+def k_ring(origin, k):
+    return sanitize_types(h3.k_ring(origin, k))
 
 
 @F.udf(returnType=T.ArrayType(T.ArrayType(T.StringType())))
-def k_ring_distances(*args, **kwargs):
-    return sanitize_types(h3.k_ring_distances(*args, **kwargs))
+def k_ring_distances(origin, k):
+    return sanitize_types(h3.k_ring_distances(origin, k))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def hex_range(*args, **kwargs):
-    return sanitize_types(h3.hex_range(*args, **kwargs))
+def hex_range(h, k):
+    return sanitize_types(h3.hex_range(h, k))
 
 
 @F.udf(returnType=T.ArrayType(T.ArrayType(T.StringType())))
-def hex_range_distances(*args, **kwargs):
-    return sanitize_types(h3.hex_range_distances(*args, **kwargs))
+def hex_range_distances(h, k):
+    return sanitize_types(h3.hex_range_distances(h, k))
 
 
 @F.udf(returnType=T.MapType(T.StringType(), T.ArrayType(T.ArrayType(T.StringType()))))
-def hex_ranges(*args, **kwargs):
-    return sanitize_types(h3.hex_ranges(*args, **kwargs))
+def hex_ranges(h, k):
+    return sanitize_types(h3.hex_ranges(h, k))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def hex_ring(*args, **kwargs):
-    return sanitize_types(h3.hex_ring(*args, **kwargs))
+def hex_ring(h, k):
+    return sanitize_types(h3.hex_ring(h, k))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def h3_line(*args, **kwargs):
-    return sanitize_types(h3.h3_line(*args, **kwargs))
+def h3_line(start, end):
+    return sanitize_types(h3.h3_line(start, end))
 
 
 @F.udf(returnType=T.IntegerType())
-def h3_distance(*args, **kwargs):
-    return sanitize_types(h3.h3_distance(*args, **kwargs))
+def h3_distance(h1, h2):
+    return sanitize_types(h3.h3_distance(h1, h2))
 
 
 @F.udf(returnType=T.ArrayType(T.IntegerType()))
-def experimental_h3_to_local_ij(*args, **kwargs):
-    return sanitize_types(h3.experimental_h3_to_local_ij(*args, **kwargs))
+def experimental_h3_to_local_ij(origin, h):
+    return sanitize_types(h3.experimental_h3_to_local_ij(origin, h))
 
 
 @F.udf(returnType=T.StringType())
-def experimental_local_ij_to_h3(*args, **kwargs):
-    return sanitize_types(h3.experimental_local_ij_to_h3(*args, **kwargs))
+def experimental_local_ij_to_h3(origin, i, j):
+    return sanitize_types(h3.experimental_local_ij_to_h3(origin, i, j))
 
 
 ###############################################################################
@@ -146,28 +131,28 @@ def experimental_local_ij_to_h3(*args, **kwargs):
 
 
 @F.udf(returnType=T.StringType())
-def h3_to_parent(*args, **kwargs):
-    return sanitize_types(h3.h3_to_parent(*args, **kwargs))
+def h3_to_parent(h, parent_res):
+    return sanitize_types(h3.h3_to_parent(h, parent_res))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def h3_to_children(*args, **kwargs):
-    return sanitize_types(h3.h3_to_children(*args, **kwargs))
+def h3_to_children(h, child_res):
+    return sanitize_types(h3.h3_to_children(h, child_res))
 
 
 @F.udf(returnType=T.StringType())
-def h3_to_center_child(*args, **kwargs):
-    return sanitize_types(h3.h3_to_center_child(*args, **kwargs))
+def h3_to_center_child(h3, child_res):
+    return sanitize_types(h3.h3_to_center_child(h3, child_res))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def compact(*args, **kwargs):
-    return sanitize_types(h3.compact(*args, **kwargs))
+def compact(hexes):
+    return sanitize_types(h3.compact(hexes))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def uncompact(*args, **kwargs):
-    return sanitize_types(h3.uncompact(*args, **kwargs))
+def uncompact(hexes, res):
+    return sanitize_types(h3.uncompact(hexes, res))
 
 
 ###############################################################################
@@ -184,8 +169,8 @@ def polyfill(polygons, res, geo_json_conformant=False):
 
 
 @F.udf(returnType=T.ArrayType(T.ArrayType(T.ArrayType(T.ArrayType(T.DoubleType())))))
-def h3_set_to_multi_polygon(*args, **kwargs):
-    return sanitize_types(h3.h3_set_to_multi_polygon(*args, **kwargs))
+def h3_set_to_multi_polygon(hexes, geo_json=False):
+    return sanitize_types(h3.h3_set_to_multi_polygon(hexes, geo_json))
 
 
 ###############################################################################
@@ -194,43 +179,43 @@ def h3_set_to_multi_polygon(*args, **kwargs):
 
 
 @F.udf(returnType=T.BooleanType())
-def h3_indexes_are_neighbors(*args, **kwargs):
-    return sanitize_types(h3.h3_indexes_are_neighbors(*args, **kwargs))
+def h3_indexes_are_neighbors(origin, destination):
+    return sanitize_types(h3.h3_indexes_are_neighbors(origin, destination))
 
 
 @F.udf(returnType=T.StringType())
-def get_h3_unidirectional_edge(*args, **kwargs):
-    return sanitize_types(h3.get_h3_unidirectional_edge(*args, **kwargs))
+def get_h3_unidirectional_edge(origin, destination):
+    return sanitize_types(h3.get_h3_unidirectional_edge(origin, destination))
 
 
 @F.udf(returnType=T.BooleanType())
-def h3_unidirectional_edge_is_valid(*args, **kwargs):
-    return sanitize_types(h3.h3_unidirectional_edge_is_valid(*args, **kwargs))
+def h3_unidirectional_edge_is_valid(edge):
+    return sanitize_types(h3.h3_unidirectional_edge_is_valid(edge))
 
 
 @F.udf(returnType=T.StringType())
-def get_origin_h3_index_from_unidirectional_edge(*args, **kwargs):
-    return sanitize_types(h3.get_origin_h3_index_from_unidirectional_edge(*args, **kwargs))
+def get_origin_h3_index_from_unidirectional_edge(edge):
+    return sanitize_types(h3.get_origin_h3_index_from_unidirectional_edge(edge))
 
 
 @F.udf(returnType=T.StringType())
-def get_destination_h3_index_from_unidirectional_edge(*args, **kwargs):
-    return sanitize_types(h3.get_destination_h3_index_from_unidirectional_edge(*args, **kwargs))
+def get_destination_h3_index_from_unidirectional_edge(edge):
+    return sanitize_types(h3.get_destination_h3_index_from_unidirectional_edge(edge))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def get_h3_indexes_from_unidirectional_edge(*args, **kwargs):
-    return sanitize_types(h3.get_h3_indexes_from_unidirectional_edge(*args, **kwargs))
+def get_h3_indexes_from_unidirectional_edge(edge):
+    return sanitize_types(h3.get_h3_indexes_from_unidirectional_edge(edge))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def get_h3_unidirectional_edges_from_hexagon(*args, **kwargs):
-    return sanitize_types(h3.get_h3_unidirectional_edges_from_hexagon(*args, **kwargs))
+def get_h3_unidirectional_edges_from_hexagon(h):
+    return sanitize_types(h3.get_h3_unidirectional_edges_from_hexagon(h))
 
 
 @F.udf(returnType=T.ArrayType(T.ArrayType(T.DoubleType())))
-def get_h3_unidirectional_edge_boundary(*args, **kwargs):
-    return sanitize_types(h3.get_h3_unidirectional_edge_boundary(*args, **kwargs))
+def get_h3_unidirectional_edge_boundary(h, geo_json=False):
+    return sanitize_types(h3.get_h3_unidirectional_edge_boundary(h, geo_json))
 
 
 ###############################################################################
@@ -239,43 +224,43 @@ def get_h3_unidirectional_edge_boundary(*args, **kwargs):
 
 
 @F.udf(returnType=T.DoubleType())
-def hex_area(*args, **kwargs):
-    return sanitize_types(h3.hex_area(*args, **kwargs))
+def hex_area(res, unit='m^2'):
+    return sanitize_types(h3.hex_area(res, unit))
 
 
 @F.udf(returnType=T.DoubleType())
-def cell_area(*args, **kwargs):
-    return sanitize_types(h3.cell_area(*args, **kwargs))
+def cell_area(h, unit='m^2'):
+    return sanitize_types(h3.cell_area(h, unit))
 
 
 @F.udf(returnType=T.DoubleType())
-def edge_length(*args, **kwargs):
-    return sanitize_types(h3.edge_length(*args, **kwargs))
+def edge_length(res, unit='m'):
+    return sanitize_types(h3.edge_length(res, unit))
 
 
 @F.udf(returnType=T.DoubleType())
-def exact_edge_length(*args, **kwargs):
-    return sanitize_types(h3.exact_edge_length(*args, **kwargs))
+def exact_edge_length(res, unit='m'):
+    return sanitize_types(h3.exact_edge_length(res, unit))
 
 
 @F.udf(returnType=T.IntegerType())
-def num_hexagons(*args, **kwargs):
-    return sanitize_types(h3.num_hexagons(*args, **kwargs))
+def num_hexagons(res):
+    return sanitize_types(h3.num_hexagons(res))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def get_res0_indexes(*args, **kwargs):
-    return sanitize_types(h3.get_res0_indexes(*args, **kwargs))
+def get_res0_indexes(res):
+    return sanitize_types(h3.get_res0_indexes(res))
 
 
 @F.udf(returnType=T.ArrayType(T.StringType()))
-def get_pentagon_indexes(*args, **kwargs):
-    return sanitize_types(h3.get_pentagon_indexes(*args, **kwargs))
+def get_pentagon_indexes(res):
+    return sanitize_types(h3.get_pentagon_indexes(res))
 
 
 @F.udf(returnType=T.DoubleType())
-def point_dist(*args, **kwargs):
-    return sanitize_types(h3.point_dist(*args, **kwargs))
+def point_dist(point1, point2, unit='m'):
+    return sanitize_types(h3.point_dist(point1, point2, unit))
 
 
 functions = getmembers(__package__, isfunction)
