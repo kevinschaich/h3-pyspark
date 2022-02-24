91c5103077b601f4a60f59cb754bf6f4c3f8272f
Kevin Schaich <kschaich@palantir.com>
2022-02-24 16:59:04 -0500

    Fix bug in index_shape function which missed hexes for long line segments (#9)
    * Fix bug in index_shape function which missed hexes for long line segments (Fixes #8)
    
    * Fix code linting errors

--- src/h3_pyspark/utils.py
+++ src/h3_pyspark/utils.py
@@ -1,4 +1,39 @@
 import json
+from shapely.geometry import LineString
+
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+
+def densify(line, step):
+    """
+    Given a line segment, return another line segment with the same start & endpoints,
+    and equally spaced sub-points based on `step` size.
+
+    All the points on the new line are guaranteed to intersect with the original line,
+    and the first and last points will be the same.
+    """
+
+    if line.length < step:
+        return line
+
+    length = line.length
+    current_distance = step
+    new_points = []
+
+    # take actual first point
+    new_points.append(line.interpolate(0.0, normalized=True))
+
+    # add points between endpoints by step size
+    while current_distance < length:
+        new_points.append(line.interpolate(current_distance))
+        current_distance += step
+
+    # take actual last point
+    new_points.append(line.interpolate(1.0, normalized=True))
+
+    return LineString(new_points)
 
 
 def sanitize_types(value):
